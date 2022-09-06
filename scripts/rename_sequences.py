917a0a76b01516a71421883a258bbd1756ee158b
dakota-hawkins <dyh0110@bu.edu>
2022-09-06 12:43:54 -0400

    accounts for multiple gene models pointing to single gene names

--- scripts/rename_sequences.py
+++ scripts/rename_sequences.py
@@ -3,7 +3,13 @@ from tokenize import Name
 from Bio import SeqIO
 import pandas as pd
 
-print("hi")
+
+def set_gene_name(row):
+    if row["name"] == "none":
+        return row.gene_id
+    return row["name"]
+
+
 if __name__ == "__main__":
     try:
         snakemake
@@ -11,9 +17,12 @@ if __name__ == "__main__":
         snakemake = None
     if snakemake is not None:
         records = list(SeqIO.parse(snakemake.input["fasta"], "fasta"))
-        seq_df = pd.read_csv(snakemake.input["csv"], index_col=0)
+        seq_df = pd.read_csv(snakemake.input["csv"])
+        name_counter = {x: 0 for x in seq_df.name.unique()}
         for each in records:
             gid = each.id.split("::")[0]
-            each.id = seq_df.loc[gid, "name"]
-            each.name = seq_df.loc[gid, "name"]
+            gene_name = seq_df.loc[gid, "name"]
+            name_counter[gene_name] += 1
+            each.id = f"{gene_name}#{name_counter[gene_name]}"
+            each.name = f"{gene_name}#{name_counter[gene_name]}"
         SeqIO.write(records, snakemake.output["fasta"], "fasta")
